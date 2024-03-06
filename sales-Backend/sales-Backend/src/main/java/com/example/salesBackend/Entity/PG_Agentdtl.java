94a7bf1690a9ffec7134563c2629e266572ad02c
Prasadi Sankalpana <prasadis@ceylife.lk>
2024-03-06 14:43:17 +0530

    add column names to the entities

--- sales-Backend/sales-Backend/src/main/java/com/example/salesBackend/Entity/PG_Agentdtl.java
+++ sales-Backend/sales-Backend/src/main/java/com/example/salesBackend/Entity/PG_Agentdtl.java
@@ -1,5 +1,6 @@
 package com.example.salesBackend.Entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -15,12 +16,25 @@ import lombok.*;
 @Table(name="PG_Agentdtl ")
 public class PG_Agentdtl {
     @Id
+    @Column(name="SoCode")
     private String SoCode;
+
+    @Column(name="Soname")
     private String Soname;
+
+    @Column(name="Branch")
     private String Branch;
+
+    @Column(name="ReportSup")
     private String ReportSup;
+
+    @Column(name="OrgLim")
     private String OrgLim;
+
+    @Column(name="TelNo")
     private String TelNo;
+
+    @Column(name="Nic")
     private String Nic;
 
 }
--- sales-Backend/sales-Backend/src/main/java/com/example/salesBackend/Entity/PG_POLICYINFO.java
+++ sales-Backend/sales-Backend/src/main/java/com/example/salesBackend/Entity/PG_POLICYINFO.java
@@ -20,23 +20,56 @@ public class PG_POLICYINFO {
     @Id
     @Column(name = "POLICY_NO")
     private String POLICY_NO;
+
+    @Column(name = "CLIENT_NO")
     private String CLIENT_NO;
+
+    @Column(name = "NAME")
     private String NAME;
+
+    @Column(name = "PLAN_NAME")
     private String PLAN_NAME;
+
+    @Column(name = "PAYMENT_MODE")
     private String PAYMENT_MODE;
+
+    @Column(name = "SUM_ASSURED")
     private BigDecimal SUM_ASSURED;
+
+    @Column(name = "RISK_DATE")
     private Date RISK_DATE;
+
     @Column(name = "PAIDUP_DATE")
     private Date PAIDUP_DATE;
+
+    @Column(name = "PREM_CESS_DATE")
     private Date PREM_CESS_DATE;
+
+    @Column(name = "TERM")
     private BigDecimal TERM;
+
+    @Column(name = "BRANCH")
     private String BRANCH;
+
+    @Column(name = "PREMIUM")
     private BigDecimal PREMIUM;
+
+    @Column(name = "PREMIUM_DUE")
     private BigDecimal PREMIUM_DUE;
+
+    @Column(name = "INTEREST")
     private BigDecimal INTEREST;
+
+    @Column(name = "SUNDRY_BALANCE")
     private BigDecimal SUNDRY_BALANCE;
+
+    @Column(name = "TOTAL_DUE")
     private BigDecimal TOTAL_DUE;
+
+    @Column(name = "POLICY_STATUS")
     private String POLICY_STATUS;
+
+    @Column(name = "ACCOUNT_BALANCE")
     private BigDecimal ACCOUNT_BALANCE;
 
 
