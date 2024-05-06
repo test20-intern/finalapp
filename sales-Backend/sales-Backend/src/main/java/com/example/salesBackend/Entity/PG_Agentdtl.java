54f2576a26bc319d296154abeacf7d414123114c
PrasadiS <158013767+PrasadiS@users.noreply.github.com>
2024-05-06 09:29:13 +0530

    Update PG_Agentdtl.java

--- sales-Backend/sales-Backend/src/main/java/com/example/salesBackend/Entity/PG_Agentdtl.java
+++ sales-Backend/sales-Backend/src/main/java/com/example/salesBackend/Entity/PG_Agentdtl.java
@@ -16,25 +16,44 @@ import lombok.*;
 @Table(name="PG_Agentdtl ")
 public class PG_Agentdtl {
     @Id
-    @Column(name="SoCode")
-    private String SoCode;
+    @Column(name="SO_CODE")
+    private String SO_CODE;
 
-    @Column(name="Soname")
-    private String Soname;
+    @Column(name="SONAME")
+    private String SONAME;
 
-    @Column(name="Branch")
-    private String Branch;
+    @Column(name="BRANCH")
+    private String BRANCH;
 
-    @Column(name="ReportSup")
-    private String ReportSup;
+    @Column(name="REPORT_SUP")
+    private String REPORT_SUP;
 
-    @Column(name="OrgLim")
-    private String OrgLim;
+    @Column(name="ORG_LIM")
+    private String ORG_LIM;
 
-    @Column(name="TelNo")
-    private String TelNo;
+    @Column(name="TEL_NO")
+    private String TEL_NO;
 
-    @Column(name="Nic")
-    private String Nic;
+    @Column(name="NIC")
+    private String NIC;
+
+    @Column(name="GROUP_CODE")
+    private String GROUP_CODE;
+
+    @Column(name="GROUP_DES")
+    private String GROUP_DES;
+
+    @Column(name="UNIT_CODE")
+    private String UNIT_CODE;
+
+    @Column(name="SO_CAT")
+    private String SO_CAT;
+
+    @Column(name="SO_TYPE")
+    private String SO_TYPE;
+
+    @Column(name="DATE_OF_APPOINTMENT")
+    private String DATE_OF_APPOINTMENT;
+
 
 }
