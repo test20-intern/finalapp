cd4bcb3af6e33cdf54d8831b5312d54f468b75d4
PrasadiS <158013767+PrasadiS@users.noreply.github.com>
2024-05-31 15:07:07 +0530

    Update PG_Agentdtl.java

--- sales-Backend/sales-Backend/src/main/java/com/example/salesBackend/Entity/PG_Agentdtl.java
+++ sales-Backend/sales-Backend/src/main/java/com/example/salesBackend/Entity/PG_Agentdtl.java
@@ -16,7 +16,7 @@ import lombok.*;
 @Table(name="PG_Agentdtl ")
 public class PG_Agentdtl {
     @Id
-    @Column(name="SO_CODE")
+    @Column(name="SOCODE")
     private String SO_CODE;
 
     @Column(name="SONAME")
@@ -25,13 +25,13 @@ public class PG_Agentdtl {
     @Column(name="BRANCH")
     private String BRANCH;
 
-    @Column(name="REPORT_SUP")
+    @Column(name="REPORTSUP")
     private String REPORT_SUP;
 
-    @Column(name="ORG_LIM")
+    @Column(name="ORGLIM")
     private String ORG_LIM;
 
-    @Column(name="TEL_NO")
+    @Column(name="TELNO")
     private String TEL_NO;
 
     @Column(name="NIC")
